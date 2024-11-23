fa0eef25d9c2e995e7acf1cf64b0d3a4771a890d
nishtha-902 <nishthakathpal902@gmail.com>
2024-11-23 21:14:31 +0530

    first commit

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -41,10 +41,7 @@ const Footer = () => {
                                 <LocalPhoneIcon /> <span>+123 4567 890</span>
                             </li>
                             <li className="footer__contact">
-                                <EmailIcon /> <span>shop@shema.com</span>
-                            </li>
-                            <li className="footer__contact">
-                                <LocationOnIcon /> <span>Addis Ababa, Ethiopia</span>
+                                <EmailIcon /> <span>shop@flipzone.com</span>
                             </li>
                         </ul>
                     </div>
@@ -73,7 +70,7 @@ const Footer = () => {
                 </div>
                 <div className="fotter__copyright__container">
                     <ul className='nav'>
-                        <li className="footer__copyright">©2022 Shema Ltd. |</li>
+                        <li className="footer__copyright">©2022 Flipzone Ltd. |</li>
                         <li className="footer__terms__condition"> | Terms & Condition |</li>
                         <li className="footer__privacy__policy">| Privacy Policy</li>
                     </ul>
